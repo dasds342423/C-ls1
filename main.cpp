43a39bc9573aab12e049f3ebe8cd69df6043d4fa
dasds342423 <astrakhantsev-v@class.ztk.org.ua>
2025-09-15 06:47:55 +0000

    new file:   .vscode/settings.json 	modified:   main.cpp 	new file:   main.py

--- main.cpp
+++ main.cpp
@@ -2,14 +2,18 @@
 #include <string>
 using namespace std;
 
-std::string func(string a) {
-    // cout << "function complete \n"; 
-    return a;
+u_int64_t temp(u_int64_t x)
+{
+    u_int64_t a = x * x; // x^2
+    u_int64_t b = a * a; // x^4
+    u_int64_t c = b * b; // x^8
+    u_int64_t d = c * c; // x^16
+    u_int64_t f = d * x; 
+    return f;
 }
 
 int main()
-
-{ 
-    cout << func("Hello world");
+{
+    cout << temp(4);
     return 0;
 }
