d9cbdac2f7a6224f5921dd6c658d102ef4bd796e
dasds342423 <astrakhantsev-v@class.ztk.org.ua>
2025-09-15 06:34:39 +0000

    new file:   .gitignore 	modified:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+std::string func(string a) {
+    // cout << "function complete \n"; 
+    return a;
+}
+
+int main()
+
+{ 
+    cout << func("Hello world");
+    return 0;
+}
